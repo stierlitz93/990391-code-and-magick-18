9939d20b3b11de317bb7891ba33bbc1ed37e3642
stierlitz93 <ak103-1993@yandex.ru>
2019-09-22 16:49:41 +0300

    без форыча

--- js/stat.js
+++ js/stat.js
@@ -8,6 +8,8 @@ var STEP_CLOUD = 10;
 var COL_WIDTH = 40;
 var COL_HEIGHT = 150;
 var COL_STEP = 50;
+var COL_WIDT_AND_STEP = 90;
+var CLOUD_X_AND_STEP = 150;
 var COLOR_STYLE = '#000';
 var COLOR_STYLE_MY = 'rgba(255, 0, 0, 1)';
 var FONT_STYLE_SIZE = '16px PT Mono';
@@ -17,9 +19,9 @@ var renderCloud = function (ctx, x, y, color) {
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
 };
 
-var getColor =function () {
- return 'hsl(240, ' + Math.random() * 100 + '%, 50%)'
-}
+var getColor = function () {
+  return 'hsl(240, ' + Math.random() * 100 + '%, 50%)';
+};
 
 var maxElement = function (arr) {
   var max = arr[0];
@@ -33,22 +35,30 @@ var maxElement = function (arr) {
   return max;
 };
 
-window.renderStatistics = function (ctx, names, times) {
-  renderCloud(ctx, CLOUD_X + STEP_CLOUD, CLOUD_Y + STEP_CLOUD, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-
+var headText = function (ctx) {
   ctx.fillStyle = COLOR_STYLE;
   ctx.font = FONT_STYLE_SIZE;
   ctx.fillText('Ура вы победили!', CLOUD_X + STEP_CLOUD + STEP_CLOUD, COL_STEP - STEP_CLOUD);
   ctx.fillText('Список результатов:', CLOUD_X + STEP_CLOUD + STEP_CLOUD, COL_STEP + STEP_CLOUD);
+};
+
+var paintCloud = function (ctx) {
+  renderCloud(ctx, CLOUD_X + STEP_CLOUD, CLOUD_Y + STEP_CLOUD, 'rgba(0, 0, 0, 0.7)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  paintCloud(ctx);
+
+  headText(ctx);
 
   var maxTime = maxElement(times);
 
   for (var i = 0; i < names.length; i++) {
     ctx.fillStyle = COLOR_STYLE;
-    ctx.fillText(names[i], CLOUD_X + COL_STEP + (COL_WIDTH + COL_STEP) * i, CLOUD_HEIGHT);
-    ctx.fillText(Math.round(times[i]), CLOUD_X + COL_STEP + (COL_WIDTH + COL_STEP) * i, CLOUD_HEIGHT - STEP_CLOUD - STEP_CLOUD - COL_HEIGHT * times[i] / maxTime - STEP_CLOUD);
+    ctx.fillText(names[i], CLOUD_X_AND_STEP + COL_WIDT_AND_STEP * i, CLOUD_HEIGHT);
+    ctx.fillText(Math.round(times[i]), CLOUD_X_AND_STEP + COL_WIDT_AND_STEP * i, CLOUD_HEIGHT - STEP_CLOUD - STEP_CLOUD - COL_HEIGHT * times[i] / maxTime - STEP_CLOUD);
     ctx.fillStyle = names[i] === 'Вы' ? COLOR_STYLE_MY : getColor();
-    ctx.fillRect(CLOUD_X + COL_STEP + (COL_WIDTH + COL_STEP) * i, CLOUD_HEIGHT - STEP_CLOUD - STEP_CLOUD, COL_WIDTH, -COL_HEIGHT * times[i] / maxTime);
+    ctx.fillRect(CLOUD_X_AND_STEP + COL_WIDT_AND_STEP * i, CLOUD_HEIGHT - STEP_CLOUD - STEP_CLOUD, COL_WIDTH, -COL_HEIGHT * times[i] / maxTime);
   }
 };
